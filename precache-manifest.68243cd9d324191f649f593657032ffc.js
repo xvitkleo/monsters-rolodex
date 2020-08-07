52251f9bd654ded8fd815ae6b752dc36ad1edd90
Joaquin <xvitkleo@gmail.com>
2020-08-06 20:22:39 -0500

    Updates

---
+++ precache-manifest.68243cd9d324191f649f593657032ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b94ba8b3b62f5d21bf28290ac5b78ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2bc379e4bac98a392d32",
+    "url": "/monsters-rolodex/static/css/main.664c3f00.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc379e4bac98a392d32",
+    "url": "/monsters-rolodex/static/js/main.db735e71.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
